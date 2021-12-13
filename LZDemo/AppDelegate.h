f7d6b90976526a2ba8cbd7289b41e4943272a005
fangrui <fangruiwork@163.com>
2021-12-13 16:09:38 +0800

    Initial Commit

---
+++ LZDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LZDemo
+//
+//  Created by 方锐 on 2021/12/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LZDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LZDemo
+//
+//  Created by 方锐 on 2021/12/13.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
